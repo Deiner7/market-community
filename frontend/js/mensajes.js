038e16ab9ab0867a273c225060c8b8f837524ccd
Deiner7 <condesantanadeiner9@gmail.com>
2026-05-05 17:43:39 -0500

    Organización del chat

--- frontend/js/mensajes.js
+++ frontend/js/mensajes.js
@@ -6,32 +6,63 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(response => response.json())
         .then(data => {
 
+            // LOGIN
             if (data.error) {
-                contenedor.innerHTML = "<p>Debes iniciar sesión</p>";
+
+                contenedor.innerHTML = `
+                    <p class="sin-productos">
+                        Debes iniciar sesión
+                    </p>
+                `;
+
                 return;
             }
 
+            // VACÍO
             if (data.length === 0) {
-                contenedor.innerHTML = "<p>No tienes mensajes</p>";
+
+                contenedor.innerHTML = `
+                    <p class="sin-productos">
+                        No tienes conversaciones todavía
+                    </p>
+                `;
+
                 return;
             }
 
+            // RECORRER
             data.forEach(mensaje => {
 
                 const div = document.createElement("div");
 
+                div.classList.add("card-mensaje");
+
                 div.innerHTML = `
-                    <p><strong>De:</strong> ${mensaje.nombre}</p>
-                    <p>${mensaje.contenido}</p>
 
-                    <button onclick="irChat(${mensaje.remitente})">
-                        Abrir chat
+                    <div class="mensaje-avatar">
+                        👤
+                    </div>
+
+                    <div class="mensaje-info">
+
+                        <h3>${mensaje.nombre}</h3>
+
+                        <p>
+                            ${mensaje.contenido}
+                        </p>
+
+                    </div>
+
+                    <button class="btn-chat"
+                        onclick="irChat(${mensaje.remitente})">
+
+                        Abrir Chat
+
                     </button>
-
-                    <hr>
                 `;
 
                 contenedor.appendChild(div);
+
             });
 
         })
@@ -39,7 +70,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
-//  FUNCIÓN PARA IR AL CHAT
+// IR AL CHAT
 function irChat(usuarioId) {
+
     window.location.href = `chat.html?usuario=${usuarioId}`;
+
 }
